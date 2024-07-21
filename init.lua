776a66ed2342a070d38cee399256be6cff62a9de
Emamul Andalib <emamul.andalib@gmail.com>
2024-07-21 08:13:53 +0200

    Minor changes related Telescope File Browser

--- init.lua
+++ init.lua
@@ -398,9 +398,9 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-      vim.keymap.set('n', '<space>fb', function()
+      vim.keymap.set('n', '<leader>fb', function()
         require('telescope').extensions.file_browser.file_browser()
-      end)
+      end, { desc = '[F]ile [B]rowser' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
@@ -655,7 +655,7 @@ require('lazy').setup({
     lazy = false,
     keys = {
       {
-        '<leader>f',
+        '<leader>ff',
         function()
           require('conform').format { async = true, lsp_fallback = true }
         end,
